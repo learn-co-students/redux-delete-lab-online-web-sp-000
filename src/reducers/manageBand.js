7007430fbecaea7a86c006446701fbf85ba2b531
blainelawson <blaine.lawson@gmail.com>
2019-08-27 11:22:36 -0400

    Done.

--- src/reducers/manageBand.js
+++ src/reducers/manageBand.js
@@ -14,7 +14,10 @@ export default function manageBand(state = {
       return { ...state, bands: [...state.bands, band] }
 
     case 'DELETE_BAND':
-      return { ...state, bands: state.bands.filter(band => band.id !== action.bandId)}
+      // return { ...state, bands: state.bands.filter(band => band.id !== action.bandId)}
+      const bands = state.bands.filter(band => band.id !== action.bandId)
+
+      return { bands }
 
     default:
       return state;

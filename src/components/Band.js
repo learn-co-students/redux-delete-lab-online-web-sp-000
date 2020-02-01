7cd4a6ba93a1358bcba2da859a6264c0e8ef9afc
atribecalledarty <arth3rs0ng@gmail.com>
2020-01-31 18:41:18 -0800

    Done.

--- src/components/Band.js
+++ src/components/Band.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+// import { connect } from 'react-redux';
 
 class Band extends Component {
-
   render() {
     return(
       <div>
-        Band Component
+        {/* {console.log(this.props.deleteBand)} */}
+        <li>{this.props.band.name}<button onClick={() => this.props.deleteBand(this.props.band.id)}>Delete</button></li>
       </div>
     );
   }

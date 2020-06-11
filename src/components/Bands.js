b221275faa186a616e2e091fae735bcd37a5571d
github username <github email address>
2020-06-10 20:52:28 -0400

    Done.

--- src/components/Bands.js
+++ src/components/Bands.js
@@ -1,35 +1,17 @@
-// import React, { Component } from 'react';
-// import Band from './Band';
-//
-//
-// const Bands = props => {
-//
-//   const bands = props.bands.map(band => <Band key={band.id} name={band.name} deleteBand={props.deleteBand}/>)
-//   //const bands = props.bands.map(band => <Band key={band.id} {...band} deleteBand={props.deleteBand}/>)
-//   return(
-//     <div>
-//       {bands}
-//     </div>
-//   );
-//
-// }
-//
-// export default Bands;
+import React, { Component } from 'react';
+import Band from './Band';
 
-
-
-import React from 'react';
-import Band from './Band'
 
 const Bands = props => {
-  const bands = props.bands.map(band => <Band key={band.id} {...band} deleteBand={props.deleteBand}/>)
 
-  return (
+
+  const bands = props.bands.map(band => <Band key={band.id} {...band} deleteBand={props.deleteBand}/>)
+  return(
     <div>
       {bands}
     </div>
   );
 
-};
+}
 
 export default Bands;
--- src/reducers/manageBand.js
+++ src/reducers/manageBand.js
@@ -1,30 +1,12 @@
-import uuid from 'react-uuid';
-//
-// export default function manageBand(state = {
-//   bands: []
-// }, action) {
-//   switch (action.type) {
-//     case 'ADD_BAND':
-//
-//       return { ...state, bands: [...state.bands, action.name] }
-//
-//     case 'DELETE_BAND':
-//       const bands = state.bands.filter(band => band.id !== action.id);
-//       return { bands }
-//
-//     default:
-//       return state;
-//   }
-// };
-
+//import uuid from 'react-uuid';
 
 export default function manageBand(state = {
   bands: []
 }, action) {
   switch (action.type) {
     case 'ADD_BAND':
-      const band = { id: uuid(), name: action.name }
-      //const band = { id: Math.random(), name: action.name }
+      //const band = { id: uuid(), name: action.name }
+      const band = { id: Math.random(), name: action.name }
       return { ...state, bands: [...state.bands, band] }
     case 'DELETE_BAND':
       const bands = state.bands.filter(band => band.id !== action.id);

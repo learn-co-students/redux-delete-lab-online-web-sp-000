9fe47c8afc37b4174e4547cf864059db43d3f2ff
jkellyphilly <joel.kelly.philly@gmail.com>
2020-06-26 09:47:20 -0400

    Done.

---
+++ src/components/Bands.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import Band from './Band';
+
+class Bands extends Component {
+  render() {
+    return (
+      <div>
+        {this.props.bands.map(band => {
+          return (
+            <Band key={band.id} id={band.id} name={band.name} deleteBand={this.props.deleteBand} />
+          )
+        })}
+      </div>
+    )
+  }
+}
+
+export default Bands
--- src/reducers/manageBand.js
+++ src/reducers/manageBand.js
@@ -1,10 +1,16 @@
+import uuid from 'uuid';
+
 export default function manageBand(state = {
   bands: []
 }, action) {
   switch (action.type) {
     case 'ADD_BAND':
 
-      return { ...state, bands: [...state.bands, action.name] }
+      return { ...state, bands: [...state.bands, { name: action.name, id: uuid() }] }
+
+    case 'DELETE_BAND':
+      const idx = state.bands.findIndex(band => band.id === action.id);
+      return {...state, bands: [...state.bands.slice(0, idx), ...state.bands.slice(idx + 1)] }
 
     default:
       return state;

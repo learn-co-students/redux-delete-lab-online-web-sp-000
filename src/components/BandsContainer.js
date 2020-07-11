dc7aa1088d7be200fd8ebf521961361dda2aa607
StephanieAlyssa <alyssa.yesilyurt@gmail.com>
2020-07-11 00:57:27 -0400

    Done.

--- src/components/BandsContainer.js
+++ src/components/BandsContainer.js
@@ -7,12 +7,13 @@ import Bands from './Bands'
 import { connect } from 'react-redux'
 
 class BandsContainer extends Component {
-  
+  /* Referenced stackoverflow for <div> </div> solution */
   render() {
     return (
       <div>
         <BandInput dispatch={this.props.dispatch} />
         <Bands dispatch={this.props.dispatch} bands={this.props.bands} />
+        
         
       </div>
     )

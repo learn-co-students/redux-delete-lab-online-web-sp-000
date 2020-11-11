3ee68d7f4f21a454179e585d948e41628d903aa3
ting682 <tchung682@gmail.com>
2020-11-11 17:59:29 -0500

    Done.

--- src/components/Bands.js
+++ src/components/Bands.js
@@ -6,9 +6,9 @@ class Bands extends React.Component {
     render() {
         let bands = this.props.bands.map(function (band) {
             return (
-                <Band band={band} key={band.id} deleteBand={this.props.deleteBand} />
+                <Band band={band} key={band.id} deleteBand={() => this.props.deleteBand(band.id)} />
             )
-        })
+        }, this)
         
         return (
             <ul>{bands}</ul>

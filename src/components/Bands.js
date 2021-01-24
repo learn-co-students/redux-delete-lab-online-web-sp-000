63a596355c4e19ed471bee27b3bad866d7ace567
Snowing222 <snowing222@gmail.com>
2021-01-23 23:35:07 -0500

    Done.

--- src/components/Bands.js
+++ src/components/Bands.js
@@ -4,12 +4,12 @@ import Band from './Band'
 class Bands extends Component {
   
   renderBands(){
-    console.log(this.props.bands)
+    console.log("hello",typeof(this.props))
 
     return(
         this.props.bands.map(band=><Band key={band.id} band={band} deleteBand = {this.props.deleteBand}/>))
   }
-
+  
   render() {
      return <div>{this.renderBands()}</div>
   }
--- src/reducers/manageBand.js
+++ src/reducers/manageBand.js
@@ -2,15 +2,16 @@ import uuid from 'uuid'
 export default function manageBand(state = {
   bands: []
 }, action) {
-  console.log(action)
+ 
   switch (action.type) {
     case 'ADD_BAND':
-      let newBand = {name: action.name,
+      const newBand = {name: action.name,
                      id: uuid()}
      
       return { ...state, bands: [...state.bands, newBand] }
     case 'DELETE_BAND':
-      return state.bands.filter(band=>(band.id !== action.id))
+      console.log(state.bands.filter(band=>(band.id !== action.id)))
+      return {bands: state.bands.filter(band=>(band.id !== action.id))}
 
     default:
       return state;

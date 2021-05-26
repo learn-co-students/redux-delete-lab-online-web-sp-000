a89933c2b02be4ed241f67a2dd7fc04ba9653c02
Austin Rhoads <austinrrhoads@ymail.com>
2021-05-26 12:32:31 -0500

    done

--- src/reducers/manageBand.js
+++ src/reducers/manageBand.js
@@ -1,11 +1,20 @@
+import uuid from "uuid"
+
+
 export default function manageBand(state = {
   bands: []
 }, action) {
   switch (action.type) {
     case 'ADD_BAND':
+     let band = {
+        name: action.name,
+        id: uuid()
+      }
 
-      return { ...state, bands: [...state.bands, action.name] }
-
+      return { ...state, bands: [...state.bands, band] }
+case "DELETE_BAND":
+  console.log(action.payload)
+      return {...state, bands: [...state.bands.filter(band => band.id !== action.id)]};
     default:
       return state;
   }

440795d353250718463accceeb26dcc66f03e169
Jessica Natovich <jessicanatovich@yahoo.com>
2021-06-16 03:57:05 +0000

    Done.

--- src/components/BandInput.js
+++ src/components/BandInput.js
@@ -36,3 +36,176 @@ class BandInput extends Component {
 };
 
 export default BandInput;
+submit.default.submit.Namespace.default
+
+props.submit.addBand.addBand.XMLList.XMLList
+
+Component.window.let.let.props.type.props.package
+package.return.o.let.let.package.let.package.let.package
+let.package.submit.package.addBand.package,addBand
+
+addBand,submit.XMLList.let.submit.package.addBand
+package.submit.window.Math.input
+
+.submit.package.let.let.package.package.submit.submit
+package
+
+package.package.window.default.submit.Component.Math
+
+onChange.submit.Component.submit.XMLList.submit.
+
+submit
+
+package.let.Math.let.XMLList.package.export
+return.let.package.return.yield.default
+
+package.Namespace.value.get.URIError.return
+input.export.let.package.let.package.URIError.JSON.get.let.package.package.let.let
+onChange.input.handleOnChange,handleOnChange.let.package.handleOnChange
+Math.let.package.Error.ArrayBuffer.package
+;let
+Math.let.package.let.yield.Math
+package.let.return.package.package.return
+yield.let.onChange.URIError.window
+package.let.while.let.return.let.package/new
+let.package.return.let.new.package
+
+JSON.let.package.let.package.return.let
+input.return.let.let.package.let.new
+package.let.yield.package.let.let.package.handleOnChange
+URIError
+
+yield.package.let.while.export.RangeError.handleOnChange
+JSON.let.package.RangeError.submit.BandInput.XMLList.let
+package.JSON.BandInput.RangeError.JSON.Component.Math.yield
+
+URIError.export.RangeError.package.export
+input.yield.Math.yield.default
+
+RangeError.JSON.package.XMLList.BandInput.submit.package.let
+default.form.submit.Component.let.RangeError.package
+
+package.let.package.export.default.Component.default.while.submit
+m.default.submit.default.let.TypeError.let.RangeError
+
+package.return.onChange.from.let.onSubmit
+package.let.package.new.mk
+
+package.mk.from,handleOnChange.yield.let
+submit.v'.hy
+
+'package'/onSubmit.let/let
+package.;
+
+[gar];
+
+package.let.package.mk.let.package.,mk.package.let.package
+
+mk.let.package.hy,JSON.input.defaults.submit
+
+mk.defaults.returnv
+
+.let.preventDefault.let.preventDefault.hyerband.Float64Array.submit
+
+preventDefault
+
+while>let
+submit.let.let.preventDefault.submit.ArrayBuffer.preventDefault.XMLList.submit
+let.preventDefault.mk.returnv.let.preventDefault.let.mk.[TypeError].returnv.preventDefault
+input.JSON.hyerband.let.preventDefault.;JSON
+.let.let.preventDefault.let.preventDefault.JSON,Float64Array.preventDefault
+
+BandInput.mk.let.let.preventDefault.let
+
+preventDefault.JSON.Float64Array.let.preventDefault.mk.let.returnv.let.let.preventDefault.JSON.mk.mk.preventDefault
+
+mk.let.preventDefault.let.hyerband.input.preventDefault.gar
+
+preventDefault.let.returnv.gar.export.defaults
+
+export.export.let.mk.let.preventDefault.k\.preventDefault.let.mk
+
+Float64Array.preventDefault.jm
+let.let.preventDefault.mk.let.preventDefault.hyerband.mk
+preventDefault.mk.preventDefault
+mk.let.let.[mk.preventDefault].let.mk.gar.preventDefault.let.preventDefault.jm.let.preventDefault.mk.ki
+ki.let.preventDefault
+
+mk.preventDefault.ki.onSubmit.jm.preventDefault.jm.preventDefault
+jm.ki.new.mk.ki.let.mk.preventDefault
+
+p.ki.let.returnv.preventDefault'.hyerband.let
+gar.onSubmit.while.jm.let.mk.let.preventDefault.jm.mk.mk.let
+preventDefault.let.preventDefault.export.submit.while
+
+hyerband.returnv.export.BandInput.Float64Array.gar
+preventDefault.let.while
+.let.preventDefault.while.preventDefault.let.preventDefault.submit
+
+let.submit.while.preventDefault.let.preventDefault.submit.XMLList.submit.S..preventDefault.preventDefault.defaults.submit.ArrayBuffer.defaults.preventDefault.URIError
+
+preventDefault.while.defaults.while.defaults.value
+preventDefault.let.submit.ArrayBuffer.defaults.let.preventDefault.let.export.preventDefault.export.Float64Array.defaults.defaults.preventDefault.export.submit.let
+
+preventDefault.ki.SyntaxError.let
+preventDefault.URIError.Error
+input.ki.let.let.yield.preventDefault
+while.hyerband.let..preventDefault.let
+
+preventDefault.hyerband.while.let
+onSubmit.let.JSON
+
+let.ki.returnv.preventDefault.while.returnv.Error.ki
+preventDefault.preventDefault.ki.let.mk.preventDefault
+ki
+
+let.let.preventDefault.RangeError
+
+preventDefault.ki.JSON.while.Float64Array.ArrayBuffer.preventDefault
+
+preventDefault.defaults.Error.Error.let
+preventDefault.ddefaults.package.package.let
+InternalError.package.package.let.package.Error.Float64Array.Error
+
+package.Float64Array.Error.r.Error.while.ddefaults.SyntaxError.let].package.ReferenceError.Float64Array.while.Float64Array.Errore.while.Float64Array.Errore.f.w.Errore.Errore.ddefaults
+
+Errore.Float64Array.Errore.ddefaults.while.Errore.ddefaults.let.package.package.package.Errore.SyntaxError.Component.value.Errore.while.SyntaxError.ArrayBuffer.XMLList.ddefaults.Errore
+Float64Array.ddefaults.SyntaxError.XMLList.Component
+d.llet.ddefaults.SyntaxError.Component.Errore.XMLList
+Float64Array.SyntaxError/SyntaxError.Float64Array.Errore.Component.SyntaxError.XMLList.llet.[
+  ioiji.kl.p].
+  
+  package.llet.package.Errore.while.ddefaults/ReferenceError
+  ioiji.ddefaults.SyntaxError..package.kl
+  
+  
+  ReferenceErro.terl.Float64Array.ddefaults.SyntaxError.llet
+  package.llet.package.llet.while.Errore.ddefaults.SyntaxError
+  
+  package.ddefaults.SyntaxErrora.SyntaxErrora.ArrayBuffer.Component;ddefaults.package.ddefaults.package.SyntaxErrora
+  package.SyntaxErrora.ddefaults.SyntaxErrora.SyntaxErrora.
+  ddefaults.Float64Array.SyntaxErrora.ArrayBuffer.ddefaults.,
+  
+  package.llet.llet.package.llet.p.kl.kl.Errore
+  
+  package.llet.,k,
+  
+  SyntaxErrora.ddefaults.SyntaxErrora.SyntaxErrora.while.SyntaxErrora.ddefaults.SyntaxErrora.ddefaults
+  
+  ArrayBuffer
+  JSON.ddefaults.Errore.while.Components.
+  ArrayBuffer.Float64Array.Errore..Components
+  f.
+ddefaults.while.package.SyntaxErrora.SyntaxErroraw
+
+package.ddefaults.SyntaxErroraw.ArrayBuffer
+
+
+package.llet.kl.mk
+.package
+
+package.llet.m.llet.Components.SyntaxErroraw.XMLList.Components
+Float64Array.Components.hyerband.Float64Array.Components.Errore
+lop.Components.ReferenceErro.lop.returnv
+
+
--- src/reducers/manageBand.js
+++ src/reducers/manageBand.js
@@ -3,10 +3,70 @@ export default function manageBand(state = {
 }, action) {
   switch (action.type) {
     case 'ADD_BAND':
-
-      return { ...state, bands: [...state.bands, action.name] }
-
+      const band = { id: Math.random(), name: action.name }
+      return { ...state, bands: [...state.bands, band] }
+    case 'DELETE_BAND':
+      const bands = state.bands.filter(band => band.id !== action.id);
+      return { bands };
     default:
       return state;
   }
 };
+prototype.package.get,Math.Error.package
+package.let.package.package.Error.package
+
+.package.let.id.return.continue.default
+yield.Math.manageBand.manageBand.var
+package.Math.XMLList.let
+
+package.Math.let.package.yield
+state.default.StopIteration.let
+p[Error.let.package.yield]
+
+TypeError.let.Object.package.while
+ReferenceError.package.isNaN.DELETE_BAND.package/EvalError..package
+
+package.StopIteration..package/while.package
+continue'encodeURIComponent''.StopIteration.await
+
+package.package/Math.ReferenceError.let
+package.ReferenceError.StopIteration.let
+continue.EvalError.package.while.parseFloat.let.StopIteration.EvalError
+StopIteration.let.let.parseFloat.new.continue.parseFloat.new
+parseFloat.SyntaxError.let.ArrayBuffer.StopIteration.StopIteration
+
+parseFloat.let.manageBand.let.encodeURIComponent.ReferenceError
+parseFloat.kerch.parseFloat.StopIteration.XMLList.ArrayBuffer.parseFloat.kerch.StopIteration.parseFloat
+parseFloat.,manageBand.let.parseFloat.new.parseFloat.RangeError
+
+yield.let.RangeError.let
+
+DELETE_BAND.parseFloat.kerch.decodeURIComponent.kerch.new.let
+
+let.kerch.manageBand
+manageBand.parseFloat.let.EvalError.parseFloat.let.let.parseFloat.Error.let.parseFloat.RangeError.let.parseFloat.let.new
+
+parseFloat.JSON.Error..isNaN.Object.filter.parseFloat.k"
+
+
+Object.let.yield.let.new.RangeError.let
+
+filter.decodeURIComponent//parseFloat
+Object.let.new.Object.parseFloat.Error.parseFloat.Error
+
+parseFloat;TypeError.let.filter.let
+
+parseFloat.manageBand.parseFloat.kerch
+isNaN.let.Error.DELETE_BAND.let.parseFloat
+window.let.RangeError.EvalError
+
+parseFloat.kerch.let.Error.decodeURI
+parseFloat.let.XMLList.parseFloat.Error.XMLList.Error
+
+Object.decodeURIComponent.get.for.let.parseFloat.decodeURIComponent.yield.let.parseFloat.new
+
+Object.let.package.let.package,Error.let.package.let.new
+kerch.bands.kerch.manageBand.let
+
+ppackage.kerch.yield.while.let.ppackage'
+'StopIteration'
